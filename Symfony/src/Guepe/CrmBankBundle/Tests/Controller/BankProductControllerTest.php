4df67acf8fa80da4e04bb3c8efee5e3649b1cdae
Pierre-Henri Delaval <ph.delaval@gmail.com>
2012-04-13 16:56:46 +0200

    change account layout for contact

--- Symfony/src/Guepe/CrmBankBundle/Tests/Controller/BankProductControllerTest.php
+++ Symfony/src/Guepe/CrmBankBundle/Tests/Controller/BankProductControllerTest.php
@@ -6,34 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BankProductControllerTest extends WebTestCase
 {
-    /*
-    public function testCompleteScenario()
+    
+    public function testAddAccount()
     {
         // Create a new client to browse the application
         $client = static::createClient();
 
+        
+        
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/bankproduct/');
+        $crawler = $client->request('GET', '/account/edit');
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
-
+		
+		/*
+        
         // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'bankproduct[field_name]'  => 'Test',
+        $form = $crawler->selectButton('Enregistrer')->form(array(
+            'account[name]'  => 'Test',
             // ... other fields to fill
         ));
 
+        
         $client->submit($form);
+        
         $crawler = $client->followRedirect();
 
+        
         // Check data in the show view
         $this->assertTrue($crawler->filter('td:contains("Test")')->count() > 0);
 
+        /*
         // Edit the entity
         $crawler = $client->click($crawler->selectLink('Edit')->link());
 
         $form = $crawler->selectButton('Edit')->form(array(
-            'bankproduct[field_name]'  => 'Foo',
+            'account[name]'  => 'Foo',
             // ... other fields to fill
         ));
 
@@ -42,13 +49,13 @@ class BankProductControllerTest extends WebTestCase
 
         // Check the element contains an attribute with value equals "Foo"
         $this->assertTrue($crawler->filter('[value="Foo"]')->count() > 0);
-
+		*/
         // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
+       // $client->submit($crawler->selectButton('Delete')->form());
+        //$crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        //$this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
     }
-    */
+    
 }
