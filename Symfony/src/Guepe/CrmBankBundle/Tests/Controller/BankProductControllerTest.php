39cc8ef262de0355bcedfd73813659d68a2f3f31
Pierre-Henri Delaval <ph.delaval@gmail.com>
2012-04-23 14:57:31 +0200

    Take care removed security....

--- Symfony/src/Guepe/CrmBankBundle/Tests/Controller/BankProductControllerTest.php
+++ Symfony/src/Guepe/CrmBankBundle/Tests/Controller/BankProductControllerTest.php
@@ -18,11 +18,21 @@ class BankProductControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/account/edit');
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
 		
-		/*
+		
         
         // Fill in the form and submit it
         $form = $crawler->selectButton('Enregistrer')->form(array(
             'account[name]'  => 'Test',
+			'account[company_statut]' => 'Personne physique',
+			'account[type]' => 'Core',
+        	'account[starting_date]' => '20/12/2012',
+        	'account[otherbank]' => 'Dexia',
+        	'account[streetnum]' => 'Rue de romsée, 48',
+        	'account[zip]' => '4565',
+        	'account[city]' => 'Liege',
+        	'account[country]' => 'BE',
+        	'account[notes]' => 'bla bla'
+        
             // ... other fields to fill
         ));
 
@@ -33,7 +43,9 @@ class BankProductControllerTest extends WebTestCase
 
         
         // Check data in the show view
-        $this->assertTrue($crawler->filter('td:contains("Test")')->count() > 0);
+        //		<span class="account-product">Test2</span>
+        
+        $this->assertTrue($crawler->filter('span.account-product:contains("Test")')->count() > 0);
 
         /*
         // Edit the entity
